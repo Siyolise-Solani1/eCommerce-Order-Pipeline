08cbfacfc149de1576c4a08f80ad1d1a8eecaec4
Siyolise-Solani1 <76071526+Siyolise-Solani1@users.noreply.github.com>
2026-01-22 08:49:27 +0000

    delete models: example

--- dbt_ecommerce/models/marts/order_status.sql
+++ dbt_ecommerce/models/marts/order_status.sql
@@ -4,11 +4,11 @@ WITH base AS (
         o.order_id, c.name as customer_name, o.order_date, s.status as order_status,
         s.shipped_at, s.delivered_at, DATEDIFF('hour',s.shipped_at,s.delivered_at) as delivery_hours
     FROM {{ ref('stg_orders') }} o
-    JOIN {{ ref('stg_customers') }} c ON o.customer_id = c.customer_id
     JOIN {{ ref('stg_shipments') }} s ON o.order_id = s.order_id
+    JOIN {{ ref('stg_customers') }} c ON o.customer_id = c.customer_id
 
 )
 
 SELECT *,
-    CASE WHEN order_status = 'shipped' and delivery_hours > 48 THEN 'delayed' ELSE order_status END AS final_status
+    CASE WHEN order_status = 'Shipped' and delivery_hours > 48 THEN 'Delayed' ELSE order_status END AS final_status
 FROM base
